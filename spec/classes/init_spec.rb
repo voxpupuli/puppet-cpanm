4c79bb7e3b4d830bec76420c0f5b240662b7893c
James McDonald <james@jamesmcdonald.com>
2016-12-12 09:57:18 +0100

    Added tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,5 +2,12 @@ require 'spec_helper'
 describe 'cpanm' do
   context 'with default values for all parameters' do
     it { should contain_class('cpanm') }
+	it { should contain_package('perl') }
+	it { should contain_package('gcc') }
+	it { should contain_package('make') }
+	it { should contain_file('/var/cache/cpanm-install')
+	  .with_source('puppet:///modules/cpanm/cpanm')
+	}
+	it { should contain_exec('/usr/bin/perl /var/cache/cpanm-install -n App::cpanminus') }
   end
 end
